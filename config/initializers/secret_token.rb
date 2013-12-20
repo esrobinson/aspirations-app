ac788e839cf5e2f74413057cf2948576e0e76548
Eric Robinson and Rory Ou <student@appacademy.io>
2013-12-20 10:13:30 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aspirations::Application.config.secret_token = 'be81228c5a387e8e1ba8e481f69453568b555053d5c4bb1d325e32dbdf6d98d56ca5a089eea86c2e576fcadb633af36af9c2c4c84959a5033135bbd020b9dd71'
